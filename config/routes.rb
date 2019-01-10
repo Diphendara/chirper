9c73e4518ad45d2692bb6387e0860a436fd9b2a0
Cristian Cañadas <cristiancvacas@gmail.com>
2019-01-10 19:18:03 +0100

    Create controller and views

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'chirps/index'
+  get 'chirps/new'
+  get 'chirps/create'
+  get 'chirps/edit'
+  get 'chirps/update'
+  get 'chirps/show'
+  get 'chirps/destroy'
+  get 'chirps/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
