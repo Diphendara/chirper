a27a022e3778a2bdd04e97dcb3b28e4f44bff15e
Cristian Cañadas <cristiancvacas@gmail.com>
2019-01-10 19:58:46 +0100

    Add controller and views to create and show index

--- app/controllers/chirps_controller.rb
+++ app/controllers/chirps_controller.rb
@@ -1,11 +1,17 @@
 class ChirpsController < ApplicationController
   def index
+    @chirps = Chirp.all
   end
 
   def new
+    @chirp = Chirp.new
   end
 
   def create
+    @chirp = Chirp.create(chirp_params)
+    @chirp.user_id = current_user.id
+    @chirp.save
+    redirect_to chirps_path
   end
 
   def edit
@@ -18,5 +24,9 @@ class ChirpsController < ApplicationController
   end
 
   def destroy
+  end
+
+  def chirp_params
+    params.require(:chirp).permit(:body, :user_id)
   end
 end
