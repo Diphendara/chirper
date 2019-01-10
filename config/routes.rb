bba503e5d39e87feda23596997903ec18df5c51d
Cristian Cañadas <cristiancvacas@gmail.com>
2019-01-10 19:21:36 +0100

    change router to set default root

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'chirps/index'
-  get 'chirps/new'
-  get 'chirps/create'
-  get 'chirps/edit'
-  get 'chirps/update'
-  get 'chirps/show'
-  get 'chirps/destroy'
-  get 'chirps/index'
+  resources :chirps
+
+  root 'chirps#index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
