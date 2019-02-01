9f93fe36421b335630576375a255cdd5908c878f
Cristian Cañadas <cristiancvacas@gmail.com>
2019-02-01 13:40:55 +0100

    Add shoulda

--- spec/models/chirp_spec.rb
+++ spec/models/chirp_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Chirp, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    it { should belong_to :user }
+  end
+  describe "validations" do
+      it { should validate_presence_of :body}
+      it { should validate_length_of(:body) > 0 } 
+  end
 end
